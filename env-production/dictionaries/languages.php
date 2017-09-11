d73b080bdaa98517d5e34be0c4be3f1dd97c33cd
root <root@ip-172-31-21-145.us-east-2.compute.internal>
2017-09-11 22:08:42 +0000

    first commit

---
+++ env-production/dictionaries/languages.php
@@ -0,0 +1,78 @@
+<?php
+//
+// Dictionary index built by the compiler
+//
+Dict::SetLanguagesList(
+array (
+  'EN US' => 
+  array (
+    'description' => 'English',
+    'localized_description' => 'English',
+  ),
+  'ZH CN' => 
+  array (
+    'description' => 'Chinese',
+    'localized_description' => '简体中文',
+  ),
+  'IT IT' => 
+  array (
+    'description' => 'Italian',
+    'localized_description' => 'Italiano',
+  ),
+  'TR TR' => 
+  array (
+    'description' => 'Turkish',
+    'localized_description' => 'Türkçe',
+  ),
+  'CS CZ' => 
+  array (
+    'description' => 'Czech',
+    'localized_description' => 'Čeština',
+  ),
+  'PT BR' => 
+  array (
+    'description' => 'Brazilian',
+    'localized_description' => 'Brazilian',
+  ),
+  'ES CR' => 
+  array (
+    'description' => 'Spanish',
+    'localized_description' => 'Español, Castellano',
+  ),
+  'DE DE' => 
+  array (
+    'description' => 'German',
+    'localized_description' => 'Deutsch',
+  ),
+  'FR FR' => 
+  array (
+    'description' => 'French',
+    'localized_description' => 'Français',
+  ),
+  'JA JP' => 
+  array (
+    'description' => 'Japanese',
+    'localized_description' => '日本語',
+  ),
+  'NL NL' => 
+  array (
+    'description' => 'Dutch',
+    'localized_description' => 'Nederlands',
+  ),
+  'RU RU' => 
+  array (
+    'description' => 'Russian',
+    'localized_description' => 'Русский',
+  ),
+  'DA DA' => 
+  array (
+    'description' => 'Danish',
+    'localized_description' => 'Dansk',
+  ),
+  'HU HU' => 
+  array (
+    'description' => 'Hungarian',
+    'localized_description' => 'Magyar',
+  ),
+)
+);
